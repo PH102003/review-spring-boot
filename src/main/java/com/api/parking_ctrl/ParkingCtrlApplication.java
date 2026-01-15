c826a750b3cf46ae03a8485a32cd4b3629ac4fd7
PH102003 <pedroh.rj2003@hotmail.com>
2026-01-15 18:56:51 -0300

    First commit

---
+++ src/main/java/com/api/parking_ctrl/ParkingCtrlApplication.java
@@ -0,0 +1,13 @@
+package com.api.parking_ctrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingCtrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingCtrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/parking_ctrl/ParkingCtrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parking_ctrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingCtrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
